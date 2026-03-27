b76836116a444dbbcc9a1c370e7c7fac6950e40a
Remi Huigen <remihuigen@proton.me>
2026-03-27 02:00:37 +0100

    fix: Move static llms gen to nuxt hook, reenable image route

--- server/routes/images/[...pathname].get.ts
+++ server/routes/images/[...pathname].get.ts
@@ -1,10 +1,13 @@
-// import { createError, eventHandler, getRouterParam } from 'h3'
-// import { blob } from 'hub:blob'
+import { createError, eventHandler, getRouterParam } from 'h3'
+import { blob } from 'hub:blob'
 
-// export default eventHandler(async (_event) => {
-// const pathname = getRouterParam(event, 'pathname')
-// if (!pathname) {
-// 	throw createError({ statusCode: 404, statusMessage: 'Not Found' })
-// }
-// return blob.serve(event, pathname)
-// })
+export default eventHandler(async (event) => {
+	const pathname = getRouterParam(event, 'pathname')
+
+	console.log('pathname')
+	if (!pathname) {
+		throw createError({ statusCode: 404, statusMessage: 'Not Found' })
+	}
+	console.log(await blob.list())
+	return blob.serve(event, pathname)
+})
